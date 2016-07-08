059ab529b05d0640e7099e307878db58d6f2ffc9
Yuhuang Hu <duguyue100@gmail.com>
2016-07-08 17:52:00 +0200

    update board test

---
+++ scripts/test-board.py
@@ -0,0 +1,23 @@
+"""Test script for the game board.
+
+Author: Yuhuang Hu
+Email : duguyue100@gmail.com
+"""
+
+from __future__ import print_function
+from minesweeper.msgame import MSGame
+
+
+game = MSGame(10, 10, 20)
+
+game.print_board()
+
+try:
+    input = raw_input
+except NameError:
+    pass
+
+while game.game_status == 2:
+    # play move
+    move = input("Move: ")
+    game.play_move_msg(move)
