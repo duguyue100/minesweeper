0e629850bf94886f1ae2042e8c9407eade3eff5d
Yuhuang Hu <duguyue100@gmail.com>
2016-07-11 03:41:40 +0200

    update setup script

---
+++ minesweeper/__about__.py
@@ -0,0 +1,14 @@
+"""Meta data for MineSweeper.
+
+Author: Yuhuang Hu
+Email : duguyue100@gmail.com
+"""
+
+from __future__ import division, print_function, unicode_literals
+
+__all__ = ("__version__", "__author__", "__author_email__", "__url__")
+
+__version__ = "0.1.0"
+__author__ = "Yuhuang Hu"
+__author_email__ = "duguyue100@gmail.com"
+__url__ = "https://github.com/duguyue100/minesweeper"
---
+++ setup.py
@@ -0,0 +1,46 @@
+"""Setup script for the MineSweeper package.
+
+Author: Yuhuang Hu
+Email : duguyue100@gmail.com
+"""
+
+from setuptools import setup
+
+classifiers = """
+Development Status :: 3 - Alpha
+Intended Audience :: Science/Research
+Natural Language :: English
+Operating System :: OS Independent
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3.5
+Topic :: Utilities
+Topic :: Scientific/Engineering
+Topic :: Scientific/Engineering :: Artificial Intelligence
+Topic :: Scientific/Engineering :: Visualization
+Topic :: Software Development :: Libraries :: Python Modules
+License :: OSI Approved :: MIT License
+"""
+
+try:
+    from simretina import __about__
+    about = __about__.__dict__
+except ImportError:
+    about = dict()
+    exec(open("minesweeper/__about__.py").read(), about)
+
+setup(
+    name='minesweeper',
+    version=about["__version__"],
+
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+
+    url=about["__url__"],
+    download_url=about["__download_url__"],
+
+    packages=["minesweeper"],
+
+    classifiers=list(filter(None, classifiers.split('\n'))),
+    description="A python Minesweeper with interfaces for "
+                "Reinforcement Learning."
+)
