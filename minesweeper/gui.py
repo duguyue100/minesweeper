127f830f9179daa5abf3d04064e52eaae2367443
Yuhuang Hu <duguyue100@gmail.com>
2016-07-12 05:30:35 +0200

    update new GUI

--- minesweeper/gui.py
+++ minesweeper/gui.py
@@ -4,11 +4,119 @@ Author: Yuhuang Hu
 Email : duguyue100@gmail.com
 """
 
-from functools import partial
-from PyQt4 import QtGui  # QtCore
+from PyQt4 import QtGui, QtCore
 
 FIELD_LOCATION = None
 CLICKED_FIELD = None
+
+
+class GameWidget(QtGui.QTabWidget):
+    """Setup Game Interface."""
+
+    def __init__(self, ms_game):
+        """Init the field."""
+        super(GameWidget, self).__init__()
+
+        self.ms_game = ms_game
+        self.init_ui()
+
+    def init_ui(self):
+        """Init game interface."""
+        board_width = self.ms_game.board_width
+        board_height = self.ms_game.board_height
+        self.grid_layout, self.grid_wgs = create_grid(board_width,
+                                                      board_height)
+        self.setLayout(self.grid_layout)
+
+    def update_grid(self):
+        """update grid according to info map."""
+        info_map = self.ms_game.get_info_map()
+        for i in xrange(self.ms_game.board_height):
+            for j in xrange(self.ms_game.board_width):
+                self.grid_wgs[(i, j)].info_label(info_map[i, j])
+
+
+class FieldWidget(QtGui.QLabel):
+    """A customized Field Widget."""
+
+    def __init__(self):
+        """Init the field."""
+        super(FieldWidget, self).__init__()
+
+        self.init_ui()
+
+    def init_ui(self):
+        """init the ui."""
+        self.setFixedSize(25, 25)
+        self.setText("  ")
+        self.setStyleSheet("QLabel {background-color: blue;" +
+                           "color: red;" +
+                           "qproperty-alignment: AlignCenter;}")
+
+    def mousePressEvent(self, event):
+        """Define mouse press event."""
+        if event.button() == QtCore.Qt.LeftButton:
+            # get label position
+            p_wg = self.parent()
+            p_layout = p_wg.layout()
+            idx = p_layout.indexOf(self)
+            loc = p_layout.getItemPosition(idx)[:2]
+            p_wg.ms_game.play_move("click", loc[1], loc[0])
+            p_wg.update_grid()
+        elif event.button() == QtCore.Qt.RightButton:
+            p_wg = self.parent()
+            p_layout = p_wg.layout()
+            idx = p_layout.indexOf(self)
+            loc = p_layout.getItemPosition(idx)[:2]
+            if self.text() == "@":
+                self.info_label(10)
+                p_wg.ms_game.play_move("question", loc[1], loc[0])
+                p_wg.update_grid()
+            elif self.text() == "  ":
+                self.info_label(9)
+                p_wg.ms_game.play_move("flag", loc[1], loc[0])
+                p_wg.update_grid()
+            elif self.text() == "?":
+                self.info_label(11)
+                p_wg.ms_game.play_move("unflag", loc[1], loc[0])
+                p_wg.update_grid()
+
+    def info_label(self, indicator):
+        """Set info label by given settings.
+
+        Parameters
+        ----------
+        indicator : int
+            A number where
+            0-8 is number of mines in srrounding.
+            12 is a mine field.
+        """
+        if indicator in xrange(1, 9):
+            self.setText(str(indicator))
+            self.setStyleSheet("QLabel {background-color: green;" +
+                               "color: red;" +
+                               "qproperty-alignment: AlignCenter;}")
+        elif indicator == 0:
+            self.setText(" ")
+            self.setStyleSheet("QLabel {background-color: green;" +
+                               "color: red;" +
+                               "qproperty-alignment: AlignCenter;}")
+        elif indicator == 12:
+            self.setText("!")
+            self.setStyleSheet("QLabel {background-color: black;" +
+                               "color: red;" +
+                               "qproperty-alignment: AlignCenter;}")
+        elif indicator == 9:
+            self.setText("@")
+            self.setStyleSheet("QLabel {background-color: #A3C1DA;" +
+                               "color: purple;}")
+        elif indicator == 10:
+            self.setText("?")
+            self.setStyleSheet("QLabel {background-color: yellow;" +
+                               "color: purple;}")
+        elif indicator == 11:
+            self.setText("  ")
+            self.setStyleSheet('QLabel {background-color: blue;}')
 
 
 def create_grid(grid_width, grid_height):
@@ -33,115 +141,7 @@ def create_grid(grid_width, grid_height):
     grid_wgs = {}
     for i in xrange(grid_height):
         for j in xrange(grid_width):
-            grid_wgs[(i, j)] = QtGui.QStackedWidget()
-            grid_wgs[(i, j)].setFixedSize(25, 25)
-
-            # set grids
-            temp_button = QtGui.QPushButton(" ")
-            temp_button.setFixedSize(25, 25)
-            temp_button.setStyleSheet('QPushButton {background-color: blue;}')
-            temp_button.clicked.connect(partial(button_pos, temp_button,
-                                                layout))
-            temp_label = QtGui.QLabel(" ")
-            temp_label.setFixedSize(25, 25)
-
-            grid_wgs[(i, j)].addWidget(temp_button)
-            grid_wgs[(i, j)].addWidget(temp_label)
-
+            grid_wgs[(i, j)] = FieldWidget()
             layout.addWidget(grid_wgs[(i, j)], i, j)
 
     return layout, grid_wgs
-
-
-def button_pos(button, layout):
-    """Button behavior."""
-    global FIELD_LOCATION, CLICKED_FIELD
-    parent_widget = button.parent()
-    parent_idx = layout.indexOf(parent_widget)
-    FIELD_LOCATION = layout.getItemPosition(parent_idx)[:2]
-    CLICKED_FIELD = button
-
-
-def button_move(ms_game):
-    """Transmit instruction through game."""
-    if CLICKED_FIELD is not None and FIELD_LOCATION is not None:
-        if CLICKED_FIELD.text() == " ":
-            ms_game.play_move("click", FIELD_LOCATION[1], FIELD_LOCATION[0])
-
-
-def update_grid(grid_wgs, info_map, grid_width, grid_height):
-    """Update the grid according to the info map.
-
-    Parameters
-    ----------
-    grid_wgs : Dictionary
-        collection of QStackedWidget
-    info_map : numpy.ndarray
-        the info map of the game
-    """
-    # grid_height = info_map.shape[0]
-    # grid_width = info_map.shape[1]
-
-    for i in xrange(grid_height):
-        for j in xrange(grid_width):
-            if info_map[i, j] in xrange(9) or info_map[i, j] == 12:
-                info_label(grid_wgs[(i, j)].widget(1), info_map[i, j])
-                grid_wgs[(i, j)].setCurrentIndex(1)
-            elif info_map[i, j] == 9:
-                flag_button(grid_wgs[(i, j)].widget(0))
-                grid_wgs[(i, j)].setCurrentIndex(0)
-            elif info_map[i, j] == 10:
-                question_button(grid_wgs[(i, j)].widget(0))
-                grid_wgs[(i, j)].setCurrentIndex(0)
-            elif info_map[i, j] == 11:
-                empty_button(grid_wgs[(i, j)].widget(0))
-                grid_wgs[(i, j)].setCurrentIndex(0)
-
-
-def empty_button(button):
-    """Default button style."""
-    button.setText(" ")
-    button.setStyleSheet('QPushButton {background-color: blue;}')
-
-
-def question_button(button):
-    """Question button by given settings."""
-    button.setText("?")
-    button.setStyleSheet("QPushButton {background-color: yellow;" +
-                         "color: purple;}")
-
-
-def flag_button(button):
-    """Flag button by given settings."""
-    button.setText("@")
-    button.setStyleSheet("QPushButton {background-color: #A3C1DA;" +
-                         "color: purple;}")
-
-
-def info_label(label, indicator):
-    """Set info label by given settings.
-
-    Parameters
-    ----------
-    label : QtGui.QLabel
-        a QLabel
-    indicator : int
-        A number where
-        0-8 is number of mines in srrounding.
-        12 is a mine field.
-    """
-    if indicator in xrange(1, 9):
-        label.setText(str(indicator))
-        label.setStyleSheet("QLabel {background-color: green;" +
-                            "color: red;" +
-                            "qproperty-alignment: AlignCenter;}")
-    elif indicator == 0:
-        label.setText(" ")
-        label.setStyleSheet("QLabel {background-color: green;" +
-                            "color: red;" +
-                            "qproperty-alignment: AlignCenter;}")
-    elif indicator == 12:
-        label.setText("!")
-        label.setStyleSheet("QLabel {background-color: black;" +
-                            "color: red;" +
-                            "qproperty-alignment: AlignCenter;}")
--- scripts/ms-gui.py
+++ scripts/ms-gui.py
@@ -5,13 +5,9 @@ Email : duguyue100@gmail.com
 """
 
 from __future__ import print_function
-from PyQt4 import QtGui, QtCore
-import numpy as np
+from PyQt4 import QtGui
 
-from minesweeper import MSGame
-from minesweeper import gui
-
-# create game
+from minesweeper import MSGame, gui
 
 board_width = 20
 board_height = 10
@@ -19,13 +15,11 @@ num_mines = 10
 ms_game = MSGame(board_width, board_height, num_mines,
                  port=5678, ip_add="127.0.0.1")
 
-print(ms_game.get_mine_map())
-
-# init mine sweeper application
 ms_app = QtGui.QApplication([])
 
 # define window and set layout
 ms_window = QtGui.QWidget()
+ms_window.setAutoFillBackground(True)
 ms_window.setWindowTitle("Mine Sweeper")
 ms_layout = QtGui.QGridLayout()
 ms_window.setLayout(ms_layout)
@@ -36,36 +30,10 @@ fun_wg.setLayout(fun_layout)
 reset_button = QtGui.QPushButton("Reset")
 fun_layout.addWidget(reset_button)
 
-grid_wg = QtGui.QWidget()
-grid_layout, grid_wgs = gui.create_grid(board_width, board_height)
-grid_wg.setLayout(grid_layout)
+grid_wg = gui.GameWidget(ms_game)
 
 ms_layout.addWidget(fun_wg, 0, 0)
 ms_layout.addWidget(grid_wg, 1, 0)
-
-prev_info_map = ms_game.get_info_map()
-prev_field_loc = gui.FIELD_LOCATION
-
-
-def update():
-    """Update function."""
-    global prev_info_map, prev_field_loc
-
-    if prev_field_loc != gui.FIELD_LOCATION:
-        gui.button_move(ms_game)
-        gui.update_grid(grid_wgs, ms_game.get_info_map(),
-                        board_width, board_height)
-
-    curr_info_map = ms_game.get_info_map()
-    if not np.array_equal(prev_info_map, curr_info_map):
-        prev_info_map = curr_info_map
-
-    if ms_game.game_status in [0, 1]:
-        return
-
-timer = QtCore.QTimer()
-timer.timeout.connect(update)
-timer.start(0)
 
 ms_window.show()
 ms_app.exec_()
