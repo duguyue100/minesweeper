ac59e4a7a0c8dcf7dc001faad90a5b62c3ff4770
Yuhuang Hu <duguyue100@gmail.com>
2016-07-13 15:24:50 +0200

    update for version

--- minesweeper/__about__.py
+++ minesweeper/__about__.py
@@ -8,9 +8,9 @@ from __future__ import division, print_function, unicode_literals
 
 __all__ = ("__version__", "__author__", "__author_email__", "__url__")
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Yuhuang Hu"
 __author_email__ = "duguyue100@gmail.com"
 __url__ = "https://github.com/duguyue100/minesweeper"
 __download_url__ = "https://github.com/duguyue100/" + \
-                   "minesweeper/tarball/0.1.2"
+                   "minesweeper/tarball/0.1.3"
